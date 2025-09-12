feb0e77d6612ea3123206b11306941d8969850f1
Thieu Minh <macthieuminh@gmail.com>
2025-09-12 18:34:19 +0700

    Profile Card

--- src/components/AppRoutes/index.jsx
+++ src/components/AppRoutes/index.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router"
 import Home from "../../pages/Home"
 import Counter from "../../pages/Counter"
 import TodoList from "../../pages/Todo-List"
-import ProfileCard from "../../pages/Profile-Card"
-import ProductList from "../../pages/Product-List"
+import ProfileCard from "../../pages/ProfileCard"
+import ProductList from "../../pages/ProductList"
 import Comment from "../../pages/Comment"
 import Weather from "../../pages/Weather"
 import DefaultLayout from "../../layouts/DefaultLayout"
@@ -19,12 +19,13 @@ function AppRoutes() {
                     <Route path="/counter" element={<Counter />} />
                     <Route path="/todolist" element={<TodoList />} />
                     <Route path="/comment" element={<Comment />} />
-                </Route>
-                <Route element={<SiderbarLayout />}>
                     <Route path="/profilecard" element={<ProfileCard />} />
                     <Route path="/productlist" element={<ProductList />} />
                     <Route path="/weather" element={<Weather />} />
                 </Route>
+                {/* <Route element={<SiderbarLayout />}>
+                    
+                </Route> */}
             </Routes>
         </Router>
     )
--- src/pages/Profile-Card/index.jsx
+++
@@ -1,5 +0,0 @@
-function ProfileCard() {
-    return <h1>Profile Card</h1>
-}
-
-export default ProfileCard
