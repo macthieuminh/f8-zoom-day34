0e1e80b850719c83ae7aef6b7cf153c6eed35bb1
Thieu Minh <macthieuminh@gmail.com>
2025-09-16 01:13:30 +0700

    Updates

--- src/layouts/SidebarLayout/components/Sidebar/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Sidebar.module.scss"
-
-export default function Sidebar() {
-    return (
-        <footer className={styles.sidebar}>
-            <h1>Sidebar</h1>
-        </footer>
-    )
-}
